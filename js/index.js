569916947f2b4b229ca57ff9c6e5ab389ea5d105
Alexandr <31701404+AlexandrMonakhov@users.noreply.github.com>
2020-10-02 12:19:32 +0300

    create: modal window

--- js/index.js
+++ js/index.js
@@ -25,7 +25,39 @@ let reviews = new Swiper('.reviews-slider', {
 });
 
 const menuButton = document.querySelector('.menu-button'),
-  navbarMenu = document.querySelector('.navbar-menu');
+  navbarMenu = document.querySelector('.navbar-menu'),
+  modalButton = document.querySelector('[data-toggle=modal]'),
+  modalOverlay = document.querySelector('.modal-overlay'),
+  modalDialog = document.querySelector('.modal-dialog'),
+  closeModalButton = document.querySelector('.modal__close');
+
+
+const openModal = () => {
+  modalOverlay.classList.add('modal-overlay_visible');
+  modalDialog.classList.add('modal-dialog_visible');
+};
+
+const closeModal = () => {
+  modalOverlay.classList.remove('modal-overlay_visible');
+  modalDialog.classList.remove('modal-dialog_visible');
+}
+
+const closeEsc = ({ code }) => {
+  if (code === 'Escape') {
+    closeModal();
+    document.removeEventListener('keydown', closeEsc);
+  }
+};
+
+modalButton.addEventListener('click', () => {
+  openModal();
+  document.addEventListener('keydown', closeEsc)
+});
+
+closeModalButton.addEventListener('click', () => {
+  closeModal();
+  document.removeEventListener('keydown', closeEsc);
+});
 
 menuButton.addEventListener('click', () => {
   navbarMenu.classList.toggle('navbar-menu_active');
